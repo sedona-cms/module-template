e553239147cb52dae795336af619df72e0e999f0
Ivan Kalinichenko <kalinichenko@constlab.ru>
2020-07-24 17:01:07 +0300

    wip: add bulma css for dev project

--- dev/nuxt.config.js
+++ dev/nuxt.config.js
@@ -1,11 +1,29 @@
 export default {
   rootDir: 'dev',
 
+  head: {
+    titleTemplate: '%s - Sedona CMS',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
+
   modules: ['@sedona-cms/core', '../lib/module.js'],
+
+  buildModules: ['@nuxtjs/bulma'],
 
   plugins: [{ src: '~/plugins/admin', mode: 'client' }],
 
   build: {
     extractCSS: true,
+
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false,
+        },
+      },
+    },
   },
 }
--- dev/pages/index.vue
+++ dev/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-        Index Page
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Hello World</h1>
+      <p class="subtitle">
+        My module for
+        <strong>Sedona CMS</strong>!
+      </p>
     </div>
+  </section>
 </template>
 
 <script>
-    export default {
-        name: 'IndexPage',
-    }
+export default {
+  name: 'IndexPage',
+  head: {
+    title: 'Home',
+  },
+}
 </script>
